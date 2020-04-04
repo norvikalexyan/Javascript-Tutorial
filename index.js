a2a5ebf6901d66e078e3ec699f7e532297acc37b
norvikalexyan <norvik.alexyan97@protonmail.com>
2020-04-04 14:31:18 +0400

    learning about console.log()

---
+++ index.js
@@ -0,0 +1,9 @@
+// *** 1. Console log ***
+
+//we want a log a message on a console
+console.log('Console log example: hello');
+console.warn('This is a warning message');
+console.error('This is a error message');
+var text = 'JavaScript Tutorial';
+console.log(typeof text); //this will print type of text variable.
+
