ee7625d7fee4ff42fcfd3e54fd432ab6f6f6dd9e
norvikalexyan <norvik.alexyan97@protonmail.com>
2020-07-01 20:32:56 +0400

    learning about Factory Functions in js

--- home.js
+++ home.js
@@ -557,3 +557,23 @@ const circle = {
 circle.draw(); // draw method
 
 //_____________________________________
+
+// *** 26.Factory Functions: ***
+
+function createCircle(radius) {
+	return {
+		radius, // radius: radius
+		draw() {
+			// draw: function() {}
+			console.log('draw');
+		},
+	};
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
+
+//_____________________________________
